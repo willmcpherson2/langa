5eebb3a9d1f2e73b93133d9095bdbdd12b6ecb53
William McPherson <willmcpherson2@gmail.com>
2024-07-23 23:01:28 +1000

    Use uniplate to initialise inference variables

--- app/Ast.hs
+++ app/Ast.hs
@@ -34,6 +34,7 @@ module Ast
   )
 where
 
+import Data.Data (Data)
 import Data.List.NonEmpty (NonEmpty (..))
 
 --------------------------------------------------------------------------------
@@ -46,21 +47,21 @@ data Item
   = ItemGlobal Global
   | ItemLocal Local
   | ItemNone Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data Global
   = Global Var Typed Loc -- (<- var typed)
   | GlobalZero Loc
   | GlobalOne Loc
   | GlobalMore Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data Local
   = Local Var Typed Loc -- (= var typed)
   | LocalZero Loc
   | LocalOne Loc
   | LocalMore Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 --------------------------------------------------------------------------------
 
@@ -69,23 +70,23 @@ data Typed
   | TypedFor For
   | TypedType Type
   | TypedNone Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data Ann
   = Ann Typed Exp Loc -- (: typed exp)
   | AnnZero Loc
   | AnnOne Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data For
   = For Var Exp Typed Loc -- (A var exp typed)
   | ForZero Loc
   | ForOne Loc
   | ForTwo Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 newtype Type = Type Loc -- Type
-  deriving (Show)
+  deriving (Show, Data)
 
 --------------------------------------------------------------------------------
 
@@ -112,101 +113,101 @@ data Exp
   | ExpNil NilLit
   | ExpVar Var
   | ExpNone Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data Set
   = Set Typed Typed Loc -- (Set typed typed)
   | SetZero Loc
   | SetOne Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data FunType
   = FunType Typed Typed Loc -- (=> typed typed)
   | FunTypeZero Loc
   | FunTypeOne Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data DoType
   = DoType Typed Loc -- (Do typed)
   | DoTypeZero Loc
   | DoTypeMany Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 newtype FloatType = FloatType Loc -- Float
-  deriving (Show)
+  deriving (Show, Data)
 
 newtype IntType = IntType Loc -- Int
-  deriving (Show)
+  deriving (Show, Data)
 
 newtype NatType = NatType Loc -- Nat
-  deriving (Show)
+  deriving (Show, Data)
 
 newtype CharType = CharType Loc -- Char
-  deriving (Show)
+  deriving (Show, Data)
 
 data Infer = Infer Int Loc -- #
-  deriving (Show)
+  deriving (Show, Data)
 
 data Fun
   = Fun Typed Typed Loc -- (-> typed typed)
   | FunZero Loc
   | FunOne Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data Case
   = Case Typed Typed Loc -- (? typed typed)
   | CaseZero Loc
   | CaseOne Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data Do
   = Do Typed Typed Typed Loc -- (do typed typed typed)
   | DoZero Loc
   | DoOne Loc
   | DoTwo Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data Let
   = Let Typed Typed Typed Loc -- (= typed typed typed)
   | LetZero Loc
   | LetOne Loc
   | LetTwo Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data App
   = App Typed Typed Loc -- (typed typed)
   | AppZero Loc
   | AppOne Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data Cons
   = Cons Typed Typed Loc -- [typed typed]
   | ConsZero Loc
   | ConsOne Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 data FloatLit = FloatLit Float Loc -- 3.14
-  deriving (Show)
+  deriving (Show, Data)
 
 data IntLit = IntLit Int Loc -- -7
-  deriving (Show)
+  deriving (Show, Data)
 
 data NatLit = NatLit Int Loc -- 42
-  deriving (Show)
+  deriving (Show, Data)
 
 data CharLit = CharLit Char Loc -- 'a'
-  deriving (Show)
+  deriving (Show, Data)
 
 data SymLit = SymLit String Loc -- :foo
-  deriving (Show)
+  deriving (Show, Data)
 
 newtype NilLit = NilLit Loc -- nil
-  deriving (Show)
+  deriving (Show, Data)
 
 data Var
   = Var Chars Loc -- x
   | VarNone Loc
-  deriving (Show)
+  deriving (Show, Data)
 
 --------------------------------------------------------------------------------
 
--- app/Check.hs
+++ app/Check.hs
@@ -1,6 +1,17 @@
 module Check (checkAst) where
 
 import Ast
+import Control.Monad.State
+import Data.Generics.Uniplate.Data
 
 checkAst :: Ast -> Ast
-checkAst = id
+checkAst = initInfers
+
+initInfers :: Ast -> Ast
+initInfers ast = evalState (transformBiM updateInfer ast) 1
+  where
+    updateInfer :: Infer -> State Int Infer
+    updateInfer (Infer _ loc) = do
+      n <- get
+      put $ n + 1
+      pure $ Infer n loc
