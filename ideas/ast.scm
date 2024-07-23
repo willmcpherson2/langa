53c61ec245114e8174cbc59ba51fda5e6baeaefe
William McPherson <willmcpherson2@gmail.com>
2024-07-23 22:14:31 +1000

    Use scheme extension

---
+++ ideas/ast.scm
@@ -0,0 +1,30 @@
+;; item
+(<- var typed)
+(= var typed)
+
+;; typed
+(: typed exp)
+(A var exp typed)
+Type
+
+;; exp
+(Set typed typed)
+(=> typed typed)
+(Do typed)
+Float
+Int
+Nat
+Char
+#
+(? typed typed)
+(-> typed typed)
+(do typed typed typed)
+(= typed typed typed)
+(typed typed)
+[typed typed]
+3.14
+-7
+42
+'a'
+nil
+var
